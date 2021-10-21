c37cdd73371cb839c430b404c28e04b97f233d49
ZuzaTyszka <zuzia.tyszka@vp.pl>
2021-10-21 21:32:01 +0200

    name change

--- PolygonsEditor/PolygonsEditor/MainWindow.Designer.cs
+++ PolygonsEditor/PolygonsEditor/MainWindow.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace PolygonsEditor
 {
-    partial class Form1
+    partial class MainWindow
     {
         /// <summary>
         ///  Required designer variable.
@@ -29,10 +29,18 @@ namespace PolygonsEditor
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "MainWindow";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MainWindow_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
