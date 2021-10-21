cc08ccffda7750ff84f2c67a2c0d52be437611c8
ZuzaTyszka <zuzia.tyszka@vp.pl>
2021-10-21 22:27:18 +0200

    simple layout

--- PolygonsEditor/PolygonsEditor/MainWindow.Designer.cs
+++ PolygonsEditor/PolygonsEditor/MainWindow.Designer.cs
@@ -29,21 +29,98 @@ namespace PolygonsEditor
         /// </summary>
         private void InitializeComponent()
         {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.newPolygon = new System.Windows.Forms.Button();
+            this.newCircle = new System.Windows.Forms.Button();
+            this.clearAll = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.clearAll);
+            this.splitContainer1.Panel1.Controls.Add(this.newCircle);
+            this.splitContainer1.Panel1.Controls.Add(this.newPolygon);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.pictureBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 450);
+            this.splitContainer1.SplitterDistance = 266;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(6, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(523, 447);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // newPolygon
+            // 
+            this.newPolygon.Location = new System.Drawing.Point(13, 18);
+            this.newPolygon.Name = "newPolygon";
+            this.newPolygon.Size = new System.Drawing.Size(238, 65);
+            this.newPolygon.TabIndex = 0;
+            this.newPolygon.Text = "New Polygon";
+            this.newPolygon.UseVisualStyleBackColor = true;
+            this.newPolygon.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // newCircle
+            // 
+            this.newCircle.Location = new System.Drawing.Point(13, 102);
+            this.newCircle.Name = "newCircle";
+            this.newCircle.Size = new System.Drawing.Size(238, 65);
+            this.newCircle.TabIndex = 1;
+            this.newCircle.Text = "New Circle";
+            this.newCircle.UseVisualStyleBackColor = true;
+            // 
+            // clearAll
+            // 
+            this.clearAll.Location = new System.Drawing.Point(13, 183);
+            this.clearAll.Name = "clearAll";
+            this.clearAll.Size = new System.Drawing.Size(238, 65);
+            this.clearAll.TabIndex = 2;
+            this.clearAll.Text = "Clear All";
+            this.clearAll.UseVisualStyleBackColor = true;
             // 
             // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitContainer1);
             this.Name = "MainWindow";
-            this.Text = "Form1";
+            this.Text = "Polygon Editor";
             this.Load += new System.EventHandler(this.MainWindow_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button newPolygon;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button clearAll;
+        private System.Windows.Forms.Button newCircle;
     }
 }
 
