391b5250c888428ef5d9c6c16d9415071e37ce08
ZuzaTyszka <zuzia.tyszka@vp.pl>
2021-10-24 21:56:11 +0200

    some structures defined

---
+++ PolygonsEditor/PolygonsEditor/MyEdge.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PolygonsEditor
+{
+    public class MyEdge
+    {
+        public MyPoint From { get; set; }
+        public MyPoint To { get; set; }
+        public double Degree { get { return Math.Atan((To.Y - From.Y) / (To.X - From.X)); } }
+
+        public MyEdge(MyPoint from, MyPoint to)
+        {
+            this.From = from;
+            this.To = to;
+        }
+    }
+}
---
+++ PolygonsEditor/PolygonsEditor/MyPoint.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PolygonsEditor
+{
+    public class MyPoint
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public MyPoint(double x, double y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+    }
+}
